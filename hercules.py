351c5e3c26f394c5cf6a5ac6acac3e3d723b2d1e
Raza Agha <waztaz@Waztaz-MacBook-Pro.local>
2018-06-14 17:11:04 -0400

    Storing the output in userpoint.txt file

--- hercules.py
+++ hercules.py
@@ -4,6 +4,9 @@
 import praw
 import re
 
+def BMP(s): #probably useless function now but maybe needed to print
+        return "".join((i if ord(i) < 10000 else '\ufffd' for i in s))
+
 class Hercules:
 
     redditors = []
@@ -34,9 +37,6 @@ class Hercules:
             #    comment_attributes.append([self.BMP(comment.body),comment.score,comment.id,submission.id])
             #self.commentsList.append(comment_attributes[:])
 
-    def BMP(self, s): #probably useless function now
-        return "".join((i if ord(i) < 10000 else '\ufffd' for i in s))
-
     def find(self, keyword):
         wordFound = re.compile(r'\b({0})\b'.format(keyword), flags=re.IGNORECASE).search
         for index in range(len(self.commentThread)):
@@ -49,27 +49,43 @@ class Hercules:
 #this function will get an input of a user and find all the posts mentioning a keyword and increment the value by 1 if it exists there or not.
     def userpoints(self,user,keywords):
 # a check to see if a user already exists or not. Maybe not necessary as points already makes sure of that.
-        if user in self.userpoint:
+        sub_count = 0
+        dirty_count = 0
+        with open("userpoints.txt","r") as f:
+            users_already_posted = f.read().split()
+        if user.name in users_already_posted:
             return
         else:
             self.userpoint[user] = 0
-        # storing all the comments in tempcomment for a particular user.
+        # storing 100 comments of the user in tempcomment for a particular user.
         tempcomment = []
-        for comment in self.r.redditor(user.name).comments.new(limit=None):
+
+        for comment in self.r.redditor(user.name).comments.new(limit=100):
             tempcomment.append(comment)
-        # finding all keywords in the tempcomments datastructure and incrementing it's value in the class variable userpoint.
+# finding all keywords in the tempcomments datastructure and finding percentage of dirty_words by posts.
         for x in keywords:
             wordFound = re.compile(r'\b({0})\b'.format(x), flags=re.IGNORECASE).search
             for index in range(len(tempcomment)):
                 if wordFound(tempcomment[index].body) is not None:
-                    print("Comment is ", tempcomment[index].body, " user is ", user, "and points are ", self.userpoint[user])
-                    self.userpoint[user] += 1
-            print(" \n ", user, " points = ", self.userpoint[user])
+                    print("Comment is ", BMP(tempcomment[index].body), " user is ", user, "and points are ", self.userpoint[user])
+                    # self.userpoint[user] += 1
+                    dirty_count += 1
+                sub_count += 1
+            try:
+                trashy_score = dirty_count/sub_count
+            except: trashy_score = 0.0
+            self.userpoint[user] = trashy_score
+            print(" \n ", user, " percentage points = ", self.userpoint[user])
+        with open("userpoints.txt","a") as f:
+            f.write(user.name + ' ' + ' ' + str(round(trashy_score,4)*100) + ' ' + str(sub_count) + '\n')
 
 #this function will call userpoint for all users.
     def points(self, keywords):
-        for x in set(self.redditors):
-            self.userpoints(x, keywords)
+        try:
+            for x in set(self.redditors):
+                self.userpoints(x, keywords)
+        except Exception as e:
+            print(str(e))
 
     def reply(self, commentid, replyList):
         commentid.reply(replyList)   
