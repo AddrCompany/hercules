78f08730b4522fff32948e6d856941853c198d78
Raza Agha <waztaz@Waztaz-MacBook-Pro.local>
2018-06-14 14:01:42 -0400

    Tracking users and assigning points for how useful they are

--- hercules.py
+++ hercules.py
@@ -6,10 +6,12 @@ import re
 
 class Hercules:
 
+    redditors = []
     replyCommentslist=[]
     numOfThreads=0
     subreddit_name = ""
     commentThread=[]
+    userpoint = {}
     r = praw.Reddit(user_agent='Comment Extraction (by u/showbobsandvagene69)',
                      client_id='FoLEE6kIDMG2XQ', client_secret="UfObG0klXZW4EcOWB-bMKO2AaMk",
                      username='showbobsandvagene69', password='casewestern')
@@ -37,11 +39,37 @@ class Hercules:
 
     def find(self, keyword):
         wordFound = re.compile(r'\b({0})\b'.format(keyword), flags=re.IGNORECASE).search
-        for index in range(self.numOfThreads):
+        for index in range(len(self.commentThread)):
             for comment_attributes in self.commentThread[index][0]:
                 if wordFound(comment_attributes.body) is not None:
                     print("\n*********\nWord found in this comment:",comment_attributes.id, "with thread id",comment_attributes.id )
                     self.replyCommentslist.append(comment_attributes)
+                    self.redditors.append(comment_attributes.author)
+
+#this function will get an input of a user and find all the posts mentioning a keyword and increment the value by 1 if it exists there or not.
+    def userpoints(self,user,keywords):
+# a check to see if a user already exists or not. Maybe not necessary as points already makes sure of that.
+        if user in self.userpoint:
+            return
+        else:
+            self.userpoint[user] = 0
+        # storing all the comments in tempcomment for a particular user.
+        tempcomment = []
+        for comment in self.r.redditor(user.name).comments.new(limit=None):
+            tempcomment.append(comment)
+        # finding all keywords in the tempcomments datastructure and incrementing it's value in the class variable userpoint.
+        for x in keywords:
+            wordFound = re.compile(r'\b({0})\b'.format(x), flags=re.IGNORECASE).search
+            for index in range(len(tempcomment)):
+                if wordFound(tempcomment[index].body) is not None:
+                    print("Comment is ", tempcomment[index].body, " user is ", user, "and points are ", self.userpoint[user])
+                    self.userpoint[user] += 1
+            print(" \n ", user, " points = ", self.userpoint[user])
+
+#this function will call userpoint for all users.
+    def points(self, keywords):
+        for x in set(self.redditors):
+            self.userpoints(x, keywords)
 
     def reply(self, commentid, replyList):
         commentid.reply(replyList)   
--- test.py
+++ test.py
@@ -1,8 +1,46 @@
+#import argparse
+#import sentiment_mod as s
 from hercules import Hercules
+#from google.cloud import language
 
-bot = Hercules("pakistan",30);
+def language_analysis(text):
+    client = language.LanguageServiceClient()
+    document = client.document_from_text(text)
+    sent_analysis = document.analyze_sentiment()
+    dir(sent_analysis)
+    sentiment = sent_analysis.sentiment
+
+    ent_analysis = document.analyze_entities()
+    dir(ent_analysis)
+    entities = ent_analysis.entities
+
+    return sentiment, entities
+
+#print(language.Client())
+
+bot = Hercules("startups",1000);
 
 bot.getComments()
-bot.find("Nawaz")
+bot.find("bitcoin")
+
+bot.points(["startup","bitcoin","ledger","cryptocurrencies"])
+#document = types.Document(
+#        content=content,
+#        type=enums.Document.Type.PLAIN_TEXT)
+#annotations = client.analyze_sentiment(document=document)
+
+    # Print the results
+#    print_result(annotations)
+
+for x in range(len(bot.replyCommentslist)):
+    print (bot.replyCommentslist[x].body)
+    #print(s.sentiment(bot.replyCommentslist[x].body))
+    #sentiment, entities = language_analysis(bot.replyCommentslist[x].body)
+    #print(sentiment.score, sentiment.magnitude)
+    #for e in entities:
+    #    print(e.name, e.entity_type, e.metadata, e.salience)
+
 #bot.replyCommentslist[0].body Not sure if it should be a separatefunction.
 # as the reply depends upon the comment.
+
+#print(len(bot.replyCommentslist))
